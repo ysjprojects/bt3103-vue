bd2a6da7f5ab612cc2a4df172a872fc2604ba6b9
ysjprojects <yshijie1999@gmail.com>
2022-03-08 16:37:02 +0800

    link to google maps

--- src/components/Results.vue
+++ src/components/Results.vue
@@ -1,11 +1,17 @@
 <template>
-    <div class="container-fluid" id="favTable" style="border: solid; padding:2%;">
-        This will be the Results component
-    </div>
+  <div class="container-fluid" id="favTable" style="border: solid; padding: 2%">
+    <p>Task 1 - Link to Google maps based on address given</p>
+    <p>Example: BLK 686 JURONG WEST CENTRAL 1</p>
+    <MapButton address="BLK 686 JURONG WEST CENTRAL 1" />
+  </div>
 </template>
 
 <script>
+import MapButton from "@/components/results/MapButton.vue";
 export default {
-  name: 'Results',
-}
+  name: "Results",
+  components: {
+    MapButton,
+  },
+};
 </script>
--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,21 @@ import App from './App.vue'
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap-vue/dist/bootstrap-vue.css"
 
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+/* import specific icons */
+import { faLocationDot } from '@fortawesome/free-solid-svg-icons'
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+/* add icons to the library */
+library.add(faLocationDot)
+
+/* add font awesome icon component */
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
 Vue.config.productionTip = false
 
 Vue.use(BootstrapVue)
