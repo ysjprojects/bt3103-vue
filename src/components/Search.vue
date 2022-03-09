1f177996671bcfae8af8f5f29c851ca410979819
ar4knid <72194298+ar4knid@users.noreply.github.com>
2022-03-09 10:40:43 +0800

    Form Overhaul
    Inserted Relevant fields for the form and improved the UI.
    
    Added basic string check functionality for the postal code.

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,22 +1,53 @@
 <template>
-  <div>
-    <b-form-select v-model="region" :options="regionOptions"></b-form-select>
+  <div class="container-fluid" id="favTable" style="border: solid; padding: 2%">
+    <h4>Find Carparks</h4>
+    <b-form-group
+      id="regionGroup"
+      description="Select the target region"
+      label="Select Region"
+      label-for="region"
+    >
+      <b-form-select v-model="region" :options="regionOptions"></b-form-select>
+    </b-form-group>
+    <b-form-group
+      id="postalGroup"
+      description="Enter your current Location (Postal Code)"
+      label="Current Location"
+      label-for="postalCode"
+      valid-feedback="Approved Postal Code."
+      :invalid-feedback="invalidFeedback"
+      :state="state"
+    >
+      <b-form-input
+        v-model="postalCode"
+        :state="state" trim
+      ></b-form-input>
+    </b-form-group>
   </div>
 </template>
 
 <script>
-  export default {
-    data() {
-      return {
-        region: null,
-        regionOptions: [
-          { value: 'Any', text: 'Any' },
-          { value: 'Central', text: 'Central' },
-          { value: 'East', text: 'East' },
-          { value: 'West', text: 'West'},
-          { value: 'North', text: 'North'},
-        ]
-      }
-    }
-  }
+export default {
+  computed: {
+    state() {
+      return this.postalCode.length >= 6;
+    },
+    invalidFeedback() {
+      return "Please insert a valid postal code.";
+    },
+  },
+  data() {
+    return {
+      region: "Any",
+      regionOptions: [
+        { value: "Any", text: "Any" },
+        { value: "Central", text: "Central" },
+        { value: "East", text: "East" },
+        { value: "West", text: "West" },
+        { value: "North", text: "North" },
+      ],
+      postalCode: "",
+    };
+  },
+};
 </script>
