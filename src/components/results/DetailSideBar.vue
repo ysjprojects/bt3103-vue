74051a4664df9f736db1286dca135a3cfec04ebf
br234 <brendahangjh@gmail.com>
2022-03-09 14:00:30 +0800

    Added details button and sidebar

---
+++ src/components/results/DetailSideBar.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <b-button v-b-toggle.sidebar-1>Carpark Details</b-button>
+    <b-sidebar id="sidebar-1" :title=result1.id shadow>
+      <div class="px-3 py-2">
+        <p>
+          Region: {{result1.region}}
+        </p>
+        <p>
+          Number of Lots: {{result1.numLots}}
+        </p>
+        <p>
+          Distance: {{result1.distance}}
+        </p>
+        <b-img src="https://picsum.photos/500/500/?image=54" fluid thumbnail></b-img>
+      </div>
+    </b-sidebar>
+  </div>
+</template>
+
+<script>
+export default {
+    name: "DetailSideBar",
+    props: {
+        result1: Object,
+    }
+}
+</script>
+
+<style>
+
+</style>
--- src/components/results/ResultCard.vue
+++ src/components/results/ResultCard.vue
@@ -4,6 +4,7 @@
       <b-card-title>Carpark {{ result.id }}</b-card-title>
       <b-card-text>{{ result.address }}</b-card-text>
       <MapButton address="BLK 686 JURONG WEST CENTRAL 1" />
+      <DetailSideBar :result1="this.result" />
     </b-card-body>
   </b-card>
 </template>
@@ -11,11 +12,20 @@
 
 <script>
 import MapButton from "./MapButton.vue";
+import DetailSideBar from "./DetailSideBar.vue";
 export default {
+  data(){
+    return{
+      result1: this.result,
+    };
+  },
+
   name: "ResultCard",
   components: {
     MapButton,
+    DetailSideBar,
   },
+  
   props: {
     result: Object,
   },
