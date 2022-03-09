dc5ebcb2858fbdf34ba2eaac818e7497a1e763a6
ar4knid <72194298+ar4knid@users.noreply.github.com>
2022-03-09 09:37:36 +0800

    Update Search.vue

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,5 +1,9 @@
 <template>
-  <div class="container-fluid" id="searchForm" style="border: solid; padding: 2%;">
+  <div
+    class="container-fluid"
+    id="searchForm"
+    style="border: solid; padding: 2%"
+  >
     <b-form inline>
       <label class="sr-only" for="inline-form-input-name">Name</label>
       <b-form-input
@@ -10,49 +14,61 @@
 
       <label class="sr-only" for="inline-form-input-username">Username</label>
       <b-input-group prepend="@" class="mb-2 mr-sm-2 mb-sm-0">
-        <b-form-input id="inline-form-input-username" placeholder="Username"></b-form-input>
+        <b-form-input
+          id="inline-form-input-username"
+          placeholder="Username"
+        ></b-form-input>
       </b-input-group>
 
-      <b-form-checkbox class="mb-2 mr-sm-2 mb-sm-0">Remember me</b-form-checkbox>
+      <b-form-checkbox class="mb-2 mr-sm-2 mb-sm-0"
+        >Remember me</b-form-checkbox
+      >
 
       <b-button variant="primary">Save</b-button>
     </b-form>
+    <h1>This is a test change</h1>
   </div>
 </template>
 
 <script>
-  export default {
-    name: "Search",
-    data() {
-      return {
-        form: {
-          email: '',
-          name: '',
-          food: null,
-          checked: []
-        },
-        foods: [{ text: 'Select One', value: null }, 'Carrots', 'Beans', 'Tomatoes', 'Corn'],
-        show: true
-      }
-    },
-    methods: {
-      onSubmit(event) {
-        event.preventDefault()
-        alert(JSON.stringify(this.form))
+export default {
+  name: "Search",
+  data() {
+    return {
+      form: {
+        email: "",
+        name: "",
+        food: null,
+        checked: [],
       },
-      onReset(event) {
-        event.preventDefault()
-        // Reset our form values
-        this.form.email = ''
-        this.form.name = ''
-        this.form.food = null
-        this.form.checked = []
-        // Trick to reset/clear native browser form validation state
-        this.show = false
-        this.$nextTick(() => {
-          this.show = true
-        })
-      }
-    }
-  }
+      foods: [
+        { text: "Select One", value: null },
+        "Carrots",
+        "Beans",
+        "Tomatoes",
+        "Corn",
+      ],
+      show: true,
+    };
+  },
+  methods: {
+    onSubmit(event) {
+      event.preventDefault();
+      alert(JSON.stringify(this.form));
+    },
+    onReset(event) {
+      event.preventDefault();
+      // Reset our form values
+      this.form.email = "";
+      this.form.name = "";
+      this.form.food = null;
+      this.form.checked = [];
+      // Trick to reset/clear native browser form validation state
+      this.show = false;
+      this.$nextTick(() => {
+        this.show = true;
+      });
+    },
+  },
+};
 </script>
