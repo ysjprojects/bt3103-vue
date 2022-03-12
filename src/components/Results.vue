d71fdbe1cdb8ed83029be2f9c5f264606938c9a8
ysjprojects <yshijie1999@gmail.com>
2022-03-12 11:37:51 +0800

    added some filtering functions

--- src/components/Results.vue
+++ src/components/Results.vue
@@ -1,9 +1,14 @@
 <template>
   <b-container fluid="xl" style="border: solid; padding: 2%">
     <b-row cols="1" cols-md="2" cols-xl="3">
-      <b-col class="mb-3" v-for="result in results" :key="result.id">
+      <b-col class="mb-3" v-for="result in renderedResults" :key="result.id">
         <ResultCard :result="result" />
       </b-col>
+    </b-row>
+    <b-row v-if="!hasReachedEnd">
+      <b-button @click="renderMoreResults()" variant="secondary"
+        >Read more</b-button
+      >
     </b-row>
   </b-container>
 </template>
@@ -20,9 +25,41 @@ export default {
     details: Array,
   },
 
+  data() {
+    return {
+      RENDER_COUNT: 48,
+      MAX_DISTANCE: 5,
+      currentLocationFilter: true,
+      availabilitySortDesc: true,
+      includeZeroLots: false,
+      renderedResults: [],
+    };
+  },
+
+  methods: {
+    setcurrentLocationFilter: function (bool) {
+      this.currentLocationFilter = bool;
+    },
+    renderMoreResults: function () {
+      let startIndex = this.renderedResults.length;
+      let endIndex =
+        this.results.length - this.RENDER_COUNT > this.renderedResults.length
+          ? startIndex + this.RENDER_COUNT - 1
+          : this.results.length - 1;
+
+      for (let i = startIndex; i <= endIndex; i++) {
+        this.renderedResults.push(this.results[i]);
+      }
+    },
+  },
+
+  mounted() {
+    this.renderMoreResults();
+  },
+
   computed: {
     results: function () {
-      return this.details.map((d) => {
+      let res = this.details.map((d) => {
         let distance = Math.floor(Math.random() * 30);
         let capacity = Math.floor(Math.random() * 500);
         let numLots = Math.floor(Math.random() * capacity);
@@ -34,6 +71,41 @@ export default {
           numLots: numLots,
         };
       });
+
+      if (this.currentLocationFilter) {
+        res = res.filter((d) => {
+          return d.distance <= this.MAX_DISTANCE;
+        });
+      }
+
+      if (!this.includeZeroLots) {
+        res = res.filter((d) => {
+          return d.numLots > 0;
+        });
+      }
+
+      if (this.availabilitySortDesc) {
+        res = res.sort((a, b) =>
+          (a.numLots * 100) / a.capacity < (b.numLots * 100) / b.capacity
+            ? 1
+            : -1
+        );
+      } else {
+        res = res.sort((a, b) =>
+          (a.numLots * 100) / a.capacity < (b.numLots * 100) / b.capacity
+            ? -1
+            : 1
+        );
+      }
+
+      console.log(res);
+
+      return res;
+    },
+    hasReachedEnd: function () {
+      return (
+        this.results.length - this.RENDER_COUNT <= this.renderedResults.length
+      );
     },
   },
 };
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-/* eslint-disable */ 
+/* eslint-disable */
 import router from "./router";
 import Vue from "vue";
 import BootstrapVue from "bootstrap-vue";
@@ -7,8 +7,19 @@ import App from "./App.vue";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-vue/dist/bootstrap-vue.css";
 
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+/* import specific icons */
+import { faRoute, faSquareParking, faTrashCan, fas } from '@fortawesome/free-solid-svg-icons'
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
 Vue.config.productionTip = false;
 
+library.add(faRoute, faSquareParking, faTrashCan, fas)
+Vue.component('font-awesome-icon', FontAwesomeIcon);
 Vue.use(BootstrapVue);
 
 new Vue({
