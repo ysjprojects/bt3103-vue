db1eeeaab0d3021b149163ac862942211a58c441
ysjprojects <yshijie1999@gmail.com>
2022-03-13 22:18:50 +0800

    minor fixes

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -9,20 +9,32 @@
       <b-navbar-nav>
         <b-nav-item href="/">Home</b-nav-item>
       </b-navbar-nav>
-      <div>
-        <b-button v-b-modal.modal-center variant="dark">Log In</b-button>
-        <b-modal id="modal-center" centered title="Welcome!" hide-footer="true">
-          <SignInPage />
-        </b-modal>
-      </div>
-      <div>
-        <b-button id="settings" v-b-modal.modal-center variant="dark">
-          <font-awesome-icon icon="fa-solid fa-gear" />
-        </b-button>
-        <b-modal id="settingsMod" centered title="Welcome!" hide-footer="true">
-          <SignInPage />
-        </b-modal>
-      </div>
+      <b-navbar-nav class="ml-auto">
+        <div>
+          <b-button v-b-modal.modal-center variant="dark">Log In</b-button>
+          <b-modal
+            id="modal-center"
+            centered
+            title="Welcome!"
+            hide-footer="true"
+          >
+            <SignInPage />
+          </b-modal>
+        </div>
+        <div>
+          <b-button id="settings" v-b-modal.modal-center variant="dark">
+            <font-awesome-icon icon="fa-solid fa-gear" />
+          </b-button>
+          <b-modal
+            id="settingsMod"
+            centered
+            title="Welcome!"
+            hide-footer="true"
+          >
+            <SignInPage />
+          </b-modal>
+        </div>
+      </b-navbar-nav>
     </b-navbar>
   </div>
 </template>
