c0465fad3b50759183d684ca8598b835b3ef070d
e0564330 <77277206+e0564330@users.noreply.github.com>
2022-03-19 10:11:42 +0800

    responsive navbar

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -5,42 +5,41 @@
     <router-link to="/">Home</router-link>
   </div> -->
   <div>
-    <b-navbar class="fixed-top" type="dark" variant="dark">
-      <b-navbar-nav>
-        <b-nav-item href="/"> Home</b-nav-item>
-      </b-navbar-nav>
-      <b-navbar-nav>
-        <b-nav-item href="#favTable">Favourites</b-nav-item>
-      </b-navbar-nav>
-      <b-navbar-nav>
-        <b-nav-item href="#searchOptions">Search</b-nav-item>
-      </b-navbar-nav>
-      <b-navbar-nav>
-        <b-nav-item href="#resultsTop">Results</b-nav-item>
-      </b-navbar-nav>
-      <b-navbar-nav class="ml-auto">
-        <div>
-          <b-button v-b-modal.modal-center variant="dark">Log In</b-button>
-          <b-modal
-            id="modal-center"
-            centered
-            title="Welcome!"
-            hide-footer="true"
-          >
-            <SignInPage />
-          </b-modal>
-        </div>
-        <!-- <div>
-          <b-nav-item href="/Settings">
-            <font-awesome-icon icon="fa-solid fa-gear"
-          /></b-nav-item>
-        </div> -->
-        <div>
-          <b-button href="/settings" variant="dark">
-            <font-awesome-icon icon="fa-solid fa-gear"
-          /></b-button>
-        </div>
-      </b-navbar-nav>
+    <b-navbar toggleable="lg" class="fixed-top" type="dark" variant="dark">
+      <b-navbar-brand href="#">Carparks</b-navbar-brand>
+      <b-navbar-toggle target="nav-text-collapse"></b-navbar-toggle>
+      <b-collapse id="nav-text-collapse" is-nav>
+        <b-navbar-nav>
+          <b-nav-item href="/"> Home</b-nav-item>
+        </b-navbar-nav>
+        <b-navbar-nav>
+          <b-nav-item href="#favTable">Favourites</b-nav-item>
+        </b-navbar-nav>
+        <b-navbar-nav>
+          <b-nav-item href="#searchOptions">Search</b-nav-item>
+        </b-navbar-nav>
+        <b-navbar-nav>
+          <b-nav-item href="#resultsTop">Results</b-nav-item>
+        </b-navbar-nav>
+        <b-navbar-nav class="ml-auto">
+          <div>
+            <b-button v-b-modal.modal-center variant="dark">Log In</b-button>
+            <b-modal
+              id="modal-center"
+              centered
+              title="Welcome!"
+              hide-footer="true"
+            >
+              <SignInPage />
+            </b-modal>
+          </div>
+          <div>
+            <b-button href="/settings" variant="dark">
+              <font-awesome-icon icon="fa-solid fa-gear"
+            /></b-button>
+          </div>
+        </b-navbar-nav>
+      </b-collapse>
     </b-navbar>
   </div>
 </template>
