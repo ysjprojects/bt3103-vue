fa141652cc087748fb409451fe84afeeb1e283a8
bubbleyee <68147732+bubbleyee@users.noreply.github.com>
2022-03-24 12:31:56 +0800

    updated favourites table formaat

--- src/uifire.ts
+++ src/uifire.ts
@@ -5,25 +5,15 @@ import firebase from "firebase/compat/app";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-// const firebaseConfig = {
-//   apiKey: "AIzaSyBQfwzN4QNVtFmf-jlLpFtW_rFLhxoC4IU",
-//   authDomain: "bt3103-d48ba.firebaseapp.com",
-//   projectId: "bt3103-d48ba",
-//   storageBucket: "bt3103-d48ba.appspot.com",
-//   messagingSenderId: "715925829160",
-//   appId: "1:715925829160:web:777cea7ca0cc35a578271d",
-// };
-
-// huiyee firebase (for own testing)
 const firebaseConfig = {
-  apiKey: "AIzaSyDoXHLGqGAohdqrelFad3gI6cmFZDLY4zs",
-  authDomain: "carpark-60564.firebaseapp.com",
-  projectId: "carpark-60564",
-  storageBucket: "carpark-60564.appspot.com",
-  messagingSenderId: "848440803621",
-  appId: "1:848440803621:web:c4ec80f304d8bed4c65acc",
-  measurementId: "G-M7VV4R3PKR"
+  apiKey: "AIzaSyBQfwzN4QNVtFmf-jlLpFtW_rFLhxoC4IU",
+  authDomain: "bt3103-d48ba.firebaseapp.com",
+  projectId: "bt3103-d48ba",
+  storageBucket: "bt3103-d48ba.appspot.com",
+  messagingSenderId: "715925829160",
+  appId: "1:715925829160:web:777cea7ca0cc35a578271d",
 };
+
 
 // Initialize Firebase
 // const firebaseApp = initializeApp(firebaseConfig);
